4d5763b30d12a8e001c40118fa0e3479e9b1537d
Mạnh Đạt Võ <dat.vomanh@haravan.com>
2023-07-27 21:55:42 +0700

    feat: add morgan helmet and compression

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
 const { default: helmet } = require("helmet");
+const compression = require("compression");
+
 const app = express();
 
 // * init middlewares
 app.use(morgan("dev"));
-// morgan("combined")
-// morgan("common")
-// morgan("short")
-// morgan("tiny")
 app.use(helmet());
+app.use(compression());
+
 // * init DB
 
 // * init Router

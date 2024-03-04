a163327476d93c01d171e9af6be4192e1ed489a7
Mạnh Đạt Võ <dat.vomanh@haravan.com>
2024-03-04 15:00:54 +0700

    feat: first push

---
+++ src/services/checkout.service.js
@@ -0,0 +1,12 @@
+'use strict'
+
+class CheckoutService {
+    /** Login and without login */
+
+    static async checkoutReview() {
+        const { cartId, userId, shop_order_ids } = payload
+    }
+
+}
+
+module.exports = CheckoutService
